e438457d3be93c8e7b419ed86a5eb8945f933132
JaminROCK <KingJamin@hotmail.com>
2018-04-13 21:43:26 -0600

    Update ConsoleNinja.cpp
    Put functions into their respective objects.  Added 10 enemies that die when you slash them.  Level 1 ends when all enemy characters reach right side of map, or die.

--- ConsoleNinja.cpp
+++ ConsoleNinja.cpp
@@ -6,255 +6,317 @@
 
 using namespace std;
 
-enum Direction
+void Intro();
+int RNG(int);
+
+class Map
 {
-	UP, DOWN, LEFT, RIGHT, STILL
+	public:
+		string* map = new string[23];
+		//Constructor
+		Map()
+		{
+					//			 10		   20		 30		   40		 50		   60		 70		
+					// 0123456789 123456789 123456789 123456789 123456789 123456789 123456789 1234567
+			map[0]  = "##############################################################################";
+			map[1]  = "#                                                                            #";
+			map[2]  = "#                                                                            #";
+			map[3]  = "#                                                                            #";
+			map[4]  = "#                                                                            #";
+			map[5]  = "#                                                                            #";
+			map[6]  = "#                                                                            #";
+			map[7]  = "#                                                                            #";
+			map[8]  = "#                                                                            #";
+			map[9]  = "#                                                                            #";
+			map[10] = "#                                                                            #";
+			map[11] = "#                                                                            #";
+			map[12] = "#                                                                            #";
+			map[13] = "#                                                                            #";
+			map[14] = "#                                                                            #";
+			map[15] = "#                                                                            #";
+			map[16] = "#                                                                            #";
+			map[17] = "#                                                                            #";
+			map[18] = "#                                                                            #";
+			map[19] = "#                                                                            #";
+			map[20] = "#                                                                            #";
+			map[21] = "#                                                                            #";
+			map[22] = "##############################################################################";		
+		}
+
+		void Render()
+		{
+
+			system("CLS");
+			for(int i = 0; i < 23; i++)
+			{
+				cout << map[i] << endl;
+			}
+			Sleep(100);	
+		}
+		void Render(int ms)
+		{
+
+			system("CLS");
+			for(int i = 0; i < 23; i++)
+			{
+				cout << map[i] << endl;
+			}
+			Sleep(ms);	
+		}		
+
 };
 
-class Ninja
-{
-public:
-	int x;
-	int y;
-};
 
 class Enemy
 {
-public:
-	int x;
-	int y;
-	string character;
+	public:
+		int x;
+		int y;
+		string character;
+
+		Enemy()
+		{
+			x = RNG(20)+50;
+			y = RNG(14)+4;	
+			character = "a";
+		}
+		
+
+		void DrawEnemy(Map map)
+		{
+			map.map[y].replace(x,1,character);
+		}
+		void MoveEnemy(Map map)
+		{
+			if(x > 1)
+			{
+				HideEnemy(map);
+				x--;
+				DrawEnemy(map);
+			}			
+		}
+		void HideEnemy(Map map)
+		{
+			map.map[y].replace(x,1," ");
+		}
+		void deleteMe()
+		{
+			delete [] this;
+		}
 
 };
 
-int RNG(int);
-void GenerateMap();
-void Render();
-void GetUserInput();
 
-void DrawNinja(int, int);
-void HideNinja(int, int);
-void DrawNinjaSlash(int, int);
-void HideNinjaSlash(int, int);
-void DrawScore();
-void HideScore();
-void Slash();
+class Ninja
+{
+	public:
+		int x;
+		int y;
+		Ninja()
+		{
+			x = 1;
+			y = 14;
+		}
 
-void DrawEnemy();
-void HideEnemy();
-void MoveEnemies();
+		void Slash(Map map, Enemy *enemies[])
+		{
+			DrawNinjaSlash(map);
+			for(int i = 0; i < 10; i++)
+			{
+				if(x+21 >= enemies[i]->x && x+9 <= enemies[i]->x && y+3 == enemies[i]->y)
+				{
+					enemies[i]->x = 0;
+					enemies[i]->character = "#";
+				}
+			}
+			map.Render(25);
+			HideNinjaSlash(map);
+			map.Render(25);
+			DrawNinja(map);
+			map.Render(25);
+		}
 
-string* map = new string[23];
-Direction currentDirection;
+		void DrawNinja(Map map)
+		{
+			//Height = 8
+			map.map[y].replace( x,7, "    OOO");
+			map.map[y+1].replace(x,6, "   OOO");
+			map.map[y+2].replace(x,7, "  o | o");
+			map.map[y+3].replace(x,9, "{} |/| {}");
+			map.map[y+4].replace(x,6, "   /o\\");
+			map.map[y+5].replace(x,8, " o/   \\o");
+			map.map[y+6].replace(x,8, " /     \\");
+			map.map[y+7].replace(x,11,"o[]     o[]");
+		}
+		void HideNinja(Map map)
+		{
+			map.map[y].replace( x,7, "       ");
+			map.map[y+1].replace(x,6, "      ");
+			map.map[y+2].replace(x,7, "       ");
+			map.map[y+3].replace(x,9, "         ");
+			map.map[y+4].replace(x,6, "      ");
+			map.map[y+5].replace(x,8, "        ");
+			map.map[y+6].replace(x,8, "        ");
+			map.map[y+7].replace(x,11,"           ");	
+		}
+		void DrawNinjaSlash(Map map)
+		{
+			map.map[y].replace( x,7, "    OOO");
+			map.map[y+1].replace(x,6, "   OOO");
+			map.map[y+2].replace(x,7, "   O| >");
+			map.map[y+3].replace(x,9, "   |/| {>");
+					map.map[y+3].replace(x+9, 13, "o=[]::::::::>");
+			map.map[y+4].replace(x,10,"   /o\\    ");
+			map.map[y+5].replace(x,8, "    >  >");
+			map.map[y+6].replace(x,8, "   /   \\");
+			map.map[y+7].replace(x,11,"  o[]   o[]");
+		}
+		void HideNinjaSlash(Map map)
+		{
+			map.map[y+3].replace(x+9, 13, "             ");
+		}
 
+	void GetUserInput(Map map, Enemy *enemies[])
+	{
+		if (_kbhit())
+		{
+			switch(_getch())
+			{
+				case ' ':
+					Slash(map, enemies);
+					break;
+				case 'w':
+					if(y > 1)
+					{
+						HideNinja(map);
+						y = y - 1;
+						DrawNinja(map);
+						map.Render(1);
+					}
+
+					break;
+				case 'a':
+					if(x > 1)
+					{			
+						HideNinja(map);
+						x = x - 1;
+						DrawNinja(map);
+						map.Render(1);
+					}		
+					break;				
+				case 's':
+					if(y < 14)
+					{
+						HideNinja(map);
+						y = y + 1;
+						DrawNinja(map);
+						map.Render(1);
+					}			
+					break;
+				case 'd':
+					if(x < 34)
+					{
+						HideNinja(map);
+						x = x + 1;
+						DrawNinja(map);
+						map.Render(1);
+					}
+				
+					break;				
+				case 'c':
+					system("CLS");
+					exit(0);
+					break;
+			}
+		}
+	}
+};
+
+
+
+
+Map map;
 Ninja ninja;
-Enemy enemy;
+Enemy *enemies[10];
+int numberOfEnemies = 10;
 
 int main()
 {
-	enemy.character = "a";
-	enemy.x = 50;
-	enemy.y = 14;
+	srand(time(NULL));
+	for(int i = 0; i < numberOfEnemies; i++)
+	{
+		enemies[i] = new Enemy();
+	}
 
-	ninja.x = 1;
-	ninja.y = 14;
-	GenerateMap();
-	currentDirection = STILL;
-	DrawNinja(ninja.x, ninja.y);
-	DrawEnemy();
-	//HideEnemy();
-	Render();
+	Intro();
+	_getch();
 	while(true)
 	{
-		GetUserInput();
-		MoveEnemies();	
+
+		ninja.GetUserInput(map, enemies);
+		ninja.DrawNinja(map);
+		for(int i = 0; i < numberOfEnemies; i++)
+		{
+			enemies[i] -> DrawEnemy(map);
+		}
+		for(int i = 0; i < numberOfEnemies; i++)
+		{
+			enemies[i] -> MoveEnemy(map);
+		}
+		map.Render(100);
+		
+		bool checkIfEnd[numberOfEnemies];
+		for(int i = 0; i < numberOfEnemies; i++)
+		{
+			if(enemies[i]-> x == 0 || enemies[i]-> x == 1)
+			{
+				checkIfEnd[i] = true;	
+			}
+		}
+
+
+		bool allTrue = true;
+		bool allFalse = true;
+		for(int i = 0; i < numberOfEnemies; i++)
+		{
+			if(checkIfEnd[i])
+			{
+				allFalse = false;
+			}
+			else
+			{
+				allTrue = false;
+			}
+		}
+
+		if(allTrue)
+		{
+			map.Render(100);
+			cout << "\n\nlevel 1 complete";
+			exit(0);
+		}
+
 	}
 	
-
-
-	return 0;	
-}
-
-void GetUserInput()
-{
-	if (_kbhit())
-	{
-		switch(_getch())
-		{
-			case ' ':
-				Slash();
-				break;
-			case 'w':
-				if(ninja.y > 1)
-				{
-					HideNinja(ninja.x, ninja.y);
-					ninja.y = ninja.y - 1;
-					DrawNinja(ninja.x, ninja.y);
-					Render();
-				}
-
-				break;
-			case 'a':
-				if(ninja.x > 1)
-				{			
-					HideNinja(ninja.x, ninja.y);
-					ninja.x = ninja.x - 1;
-					DrawNinja(ninja.x, ninja.y);
-					Render();		
-				}		
-				break;				
-			case 's':
-				if(ninja.y < 14)
-				{
-					HideNinja(ninja.x, ninja.y);
-					ninja.y = ninja.y + 1;
-					DrawNinja(ninja.x, ninja.y);
-					Render();	
-				}			
-				break;
-			case 'd':
-				if(ninja.x < 34)
-				{
-					HideNinja(ninja.x, ninja.y);
-					ninja.x = ninja.x + 1;
-					DrawNinja(ninja.x, ninja.y);
-					Render();					
-				}
-			
-				break;				
-			case 'C':
-				system("CLS");
-				exit(0);
-				break;
-		}
-	}
-}
-void MoveEnemies()
-{
-	if(enemy.x > 0)
-	{
-		HideEnemy();
-		enemy.x = enemy.x-1;
-		DrawEnemy();
-		Render();		
-	}
-
-}
-
-void Slash()
-{
-	DrawNinjaSlash(ninja.x, ninja.y);
-	Render();
-	HideNinjaSlash(ninja.x, ninja.y);
-	Render();
-	DrawNinja(ninja.x, ninja.y);
-	Render();
-}
-void Render()
-{
-	system("CLS");
-	for(int i = 0; i < 23; i++)
-	{
-		cout << map[i] << endl;
-	}
-	Sleep(10);	
-}
-
-void DrawNinja(int x, int y)
-{
-	//Height = 8
-	map[y].replace( x,7, "    OOO");
-	map[y+1].replace(x,6, "   OOO");
-	map[y+2].replace(x,7, "  o | o");
-	map[y+3].replace(x,9, "{} |/| {}");
-	map[y+4].replace(x,6, "   /o\\");
-	map[y+5].replace(x,8, " o/   \\o");
-	map[y+6].replace(x,8, " /     \\");
-	map[y+7].replace(x,11,"o[]     o[]");
-}
-void HideNinja(int x, int y)
-{
-	map[y].replace( x,7, "       ");
-	map[y+1].replace(x,6, "      ");
-	map[y+2].replace(x,7, "       ");
-	map[y+3].replace(x,9, "         ");
-	map[y+4].replace(x,6, "      ");
-	map[y+5].replace(x,8, "        ");
-	map[y+6].replace(x,8, "        ");
-	map[y+7].replace(x,11,"           ");	
-}
-void DrawNinjaSlash(int x, int y)
-{
-	map[y].replace( x,7, "    OOO");
-	map[y+1].replace(x,6, "   OOO");
-	map[y+2].replace(x,7, "   O| >");
-	map[y+3].replace(x,9, "   |/| {>");
-			map[y+3].replace(x+9, 13, "o=[]::::::::>");
-	map[y+4].replace(x,10,"   /o\\    ");
-	map[y+5].replace(x,8, "    >  >");
-	map[y+6].replace(x,8, "   /   \\");
-	map[y+7].replace(x,11,"  o[]   o[]");
-}
-void HideNinjaSlash(int x, int y)
-{
-	map[y+3].replace(x+9, 13, "             ");
-}
-void DrawScore()
-{
-	map[1].replace(10, 7, "Score: ");
-}
-void HideScore()
-{
-	map[1].replace(0, 78, "#                                                                            #");	
-}
-
-void DrawEnemy()
-{
-	int x = enemy.x;
-	int y = enemy.y;
-	map[y].replace(x,1,enemy.character);
-}
-void HideEnemy()
-{
-	int x = enemy.x;
-	int y = enemy.y;
-	map[y].replace(x,1," ");
-}
-
-void GenerateMap()
-{
-			//			 10		   20		 30		   40		 50		   60		 70		
-			// 0123456789 123456789 123456789 123456789 123456789 123456789 123456789 1234567
-	map[0]  = "##############################################################################";
-	map[1]  = "#                                                                            #";
-	map[2]  = "#                                                                            #";
-	map[3]  = "#                                                                            #";
-	map[4]  = "#                                                                            #";
-	map[5]  = "#                                                                            #";
-	map[6]  = "#                                                                            #";
-	map[7]  = "#                                                                            #";
-	map[8]  = "#                                                                            #";
-	map[9]  = "#                                                                            #";
-	map[10] = "#                                                                            #";
-	map[11] = "#                                                                            #";
-	map[12] = "#                                                                            #";
-	map[13] = "#                                                                            #";
-	map[14] = "#                                                                            #";
-	map[15] = "#                                                                            #";
-	map[16] = "#                                                                            #";
-	map[17] = "#                                                                            #";
-	map[18] = "#                                                                            #";
-	map[19] = "#                                                                            #";
-	map[20] = "#                                                                            #";
-	map[21] = "#                                                                            #";
-	map[22] = "##############################################################################";
-
+	
+	return 0;
 }
 
 int RNG(int max)
 {
-	srand(time(NULL));
 	int randomNumber = rand() % max;
-
 	return randomNumber;
+}
+
+void Intro()
+{
+	system("CLS");
+	cout << "\n\n\n\n\n                            "
+		 << "Welcome to Console Ninja.\n\n              "
+		 << "Hack at the enemy characters until there are none left.\n\n                    "
+		 << "Use the W,A,S,D keys to move your ninja. \n                      "
+		 << "Press SPACEBAR to slash at the enemy.\n\n                      "
+		 << "At any point press type c to exit.\n\n                     "
+		 << "Press any key on the keyboard to begin.\n        \n                            "
+		 << "Good luck, warrior.\n\n                                     ";
 }
